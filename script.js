e66a7c0c8c2b70ad70fc38da40f04e1f02254a42
wagner machado <wagnermachado52020@gmail.com>
2025-12-05 21:43:54 -0300

    w
    w

--- script.js
+++ script.js
@@ -732,5 +732,12 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 });
+//sisteam web app
 
+ if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("SW registrado!"))
+      .catch(err => console.error("Erro:", err));
+  }
 
+  
